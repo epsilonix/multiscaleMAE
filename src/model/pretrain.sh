fc2586b720b1d4ff3ac19f561bd53d71cdcbb0e5
epsilonix <sterling.sam@gmail.com>
2024-05-05 17:53:58 -0400

    Update pretrain.sh

--- src/model/pretrain.sh
+++ src/model/pretrain.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH -t 7-00:00:00
+#SBATCH -t 0-01:00:00
 #SBATCH -p gpu4_long,gpu8_long
 #SBATCH -N 1
-#SBATCH --mem=200G
+#SBATCH --mem=60G
 #SBATCH --tasks-per-node=1
-#SBATCH --cpus-per-task=10
+#SBATCH --cpus-per-task=1
 #SBATCH --gres=gpu:1
 #SBATCH --job-name=pretrain_MAE
 #SBATCH --output=/gpfs/scratch/ss14424/Brain/logs/pretrain_%j.log
@@ -17,7 +17,7 @@
 torchrun \
     --standalone \
     --nnodes=1 \
-    --nproc_per_node=10 \
+    --nproc_per_node=1 \
     /gpfs/scratch/ss14424/singlecell/src/model/main_pretrain.py \
         --epoch 1 \
         --batch_size 32 \
