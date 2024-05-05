72955ffcd662e734aa072666be96a4fdbca1dfd0
epsilonix <sterling.sam@gmail.com>
2024-05-05 16:33:44 -0400

    Update run_preprocessing.sh

--- src/preprocessing/run_preprocessing.sh
+++ src/preprocessing/run_preprocessing.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-python /gpfs/scratch/ss14424/canvas-sc/src/preprocessing/preprocess.py \
+python /gpfs/scratch/ss14424/singlecell/src/preprocessing/preprocess.py \
 /gpfs/scratch/ss14424/Brain/BrainData/BRAIN_IMG_GliomaTif \
 /gpfs/scratch/ss14424/Brain/BrainData/BRAIN_IMC_CellType \
 /gpfs/scratch/ss14424/Brain/cells/img_output
