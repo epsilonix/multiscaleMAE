3ade564810b75a4f8ac969183be304ca6ca59043
epsilonix <sterling.sam@gmail.com>
2024-05-09 20:10:49 -0400

    Update pretrain.sh

--- src/model/pretrain.sh
+++ src/model/pretrain.sh
@@ -2,10 +2,10 @@
 #SBATCH -t 5-00:00:00
 #SBATCH -p gpu4_long,gpu8_long
 #SBATCH -N 1
-#SBATCH --mem=200G
+#SBATCH --mem=250G
 #SBATCH --tasks-per-node=1
-#SBATCH --cpus-per-task=10
-#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=8
+#SBATCH --gres=gpu:4
 #SBATCH --job-name=pretrain_MAE
 #SBATCH --output=/gpfs/scratch/ss14424/logs/cell_pretrain_%j.log
 
@@ -17,11 +17,11 @@ source activate /gpfs/home/ss14424/.conda/envs/canvas-env
 torchrun \
     --standalone \
     --nnodes=1 \
-    --nproc_per_node=1 \
+    --nproc_per_node=4 \
     /gpfs/scratch/ss14424/singlecell/src/model/main_pretrain.py \
         --epoch 1000 \
         --batch_size 32 \
         --tile_size 20 \
-        --output_dir "/gpfs/scratch/ss14424/Brain/cells/model_output" \
+        --output_dir "/gpfs/scratch/ss14424/Brain/cells/model_output_20" \
         --log_dir "/gpfs/scratch/ss14424/logs" \
-        --data_path "/gpfs/scratch/ss14424/Brain/cells/img_output"
+        --data_path "/gpfs/scratch/ss14424/Brain/cells/img_output_20"
