570a7659f96d54792c5afd19c5faa19e5a19363c
epsilonix <sterling.sam@gmail.com>
2024-05-10 17:13:27 -0400

    Create canvas_base_run.sh

---
+++ src/analysis/canvas_base_run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -t 0-01:00:00
+#SBATCH -p gpu4_long,gpu8_long
+#SBATCH -N 1
+#SBATCH --mem=100G
+#SBATCH --tasks-per-node=1
+#SBATCH --cpus-per-task=5
+#SBATCH --gres=gpu:1
+#SBATCH --job-name=infer_MAE
+#SBATCH --output=/gpfs/scratch/ss14424/logs/infer_%j.log
+
+source activate /gpfs/home/ss14424/.conda/envs/canvas-env
+
+python src/analysis/canvas_base.py
