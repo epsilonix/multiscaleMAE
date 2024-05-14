dbe6320bb846bb5d977c4cc9bc7975fef44b1f00
epsilonix <sterling.sam@gmail.com>
2024-05-14 14:11:31 -0400

    a

--- src/analysis/canvas_base_run.sh
+++ src/analysis/canvas_base_run.sh
@@ -9,9 +9,11 @@
 #SBATCH --job-name=infer_MAE
 #SBATCH --output=/gpfs/scratch/ss14424/logs/infer_%j.log
 
-export NUMBA_DISABLE_JIT=1
-export NUMBA_DISABLE_CACHE=1
 
 source activate /gpfs/home/ss14424/.conda/envs/canvas-env
 
 python src/analysis/canvas_base.py
+
+
+
+#srun --pty --time=0-07:00:00 --partition=gpu4_long,gpu8_long --nodes=1 --mem=150G --ntasks-per-node=1 --cpus-per-task=5 --gres=gpu:1 --job-name=canvas_base_run bash
