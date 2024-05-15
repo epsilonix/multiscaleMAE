f9bceb5ec313123d23832bc76300f0907889cad4
epsilonix <sterling.sam@gmail.com>
2024-05-15 16:43:58 -0400

    Update run_preprocessing.sh

--- src/preprocessing/run_preprocessing.sh
+++ src/preprocessing/run_preprocessing.sh
@@ -15,5 +15,5 @@ python \
 /gpfs/scratch/ss14424/singlecell/src/preprocessing/preprocess.py \
 /gpfs/scratch/ss14424/Brain/Brain_Data2/BRAIN_IMC_MaskTif \
 /gpfs/scratch/ss14424/Brain/Brain_Data2/BRAIN_IMC_CellType \
-/gpfs/scratch/ss14424/Brain/cells/img_output_10 \
-10
+/gpfs/scratch/ss14424/Brain/cells/img_output_20 \
+20
