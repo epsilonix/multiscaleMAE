84b75429f4885839c67ada0990966aca16433bf5
epsilonix <sterling.sam@gmail.com>
2024-06-11 20:37:38 -0400

    a

--- src/preprocessing/run_preprocessing.sh
+++ src/preprocessing/run_preprocessing.sh
@@ -13,7 +13,7 @@ source activate /gpfs/home/ss14424/.conda/envs/canvas-env
 
 python \
 /gpfs/scratch/ss14424/singlecell/src/preprocessing/preprocess.py \
-/gpfs/scratch/ss14424/Brain/Brain_Data2/BRAIN_IMC_MaskTif \
-/gpfs/scratch/ss14424/Brain/Brain_Data2/BRAIN_IMC_CellType \
+/gpfs/scratch/ss14424/Brain/BrainData/BRAIN_IMC_MaskTif \
+/gpfs/scratch/ss14424/Brain/BrainData/BRAIN_IMC_CellType \
 /gpfs/scratch/ss14424/Brain/cells/img_output_20 \
 20
