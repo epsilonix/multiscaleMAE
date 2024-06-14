dfae275252b47c69ada033101ea2bd47a3b1233f
epsilonix <sterling.sam@gmail.com>
2024-06-14 18:04:26 -0400

    a

--- src/model/data/imc_dataset.py
+++ src/model/data/imc_dataset.py
@@ -1,11 +1,13 @@
 import os
-
 import zarr
 import numpy as np
 import pandas as pd
 import torch.utils.data as data
-
 from model.data.slide_dataset import SlideDataset
+
+# Declare the paths to the channel txt and csv files as variables near the top
+CHANNEL_TXT_PATH = '/gpfs/scratch/ss14424/Brain/common_channels_glioma.txt'
+CHANNEL_CSV_PATH = '/gpfs/scratch/ss14424/Brain/channels_glioma.csv'
 
 class NPYDataset(SlideDataset):
 
@@ -13,7 +15,6 @@ class NPYDataset(SlideDataset):
         super().__init__(root_path, tile_size, transform)
         self.slide = self.read_slide(root_path, lazy)
         self.read_counter = 0
-
 
     def read_slide(self, file_path, lazy):
         ''' Read numpy file on disk mapped to memory '''
@@ -25,8 +26,6 @@ class NPYDataset(SlideDataset):
         return slide
 
     def read_region(self, pos_x, pos_y, width, height):
-        #print(f'imc_dataset slide size: {self.slide.shape}')
-        #print(f"imc dataset pos_x: {pos_x}, pos_y: {pos_y}, width: {width}, height: {height}")
         ''' Read a numpy slide region '''
         
         pos_x = int(pos_x)
@@ -58,7 +57,7 @@ class NPYDataset(SlideDataset):
         dims = self.get_slide_dimensions()
         cache = np.zeros((dims[0] // scaling_factor, dims[1] // scaling_factor, 4), dtype = np.uint8)
         for x in tqdm(range(0, dims[0], tile_size)):
-            for y in range(0, dims[1], tile_size):
+            for y in tqdm(range(0, dims[1], tile_size)):
                 tile = self.read_region(x, y, tile_size, tile_size).swapaxes(0, 1)
                 reduced_tile = block_reduce(tile, block_size=(scaling_factor, scaling_factor, 1), func=np.mean)
                 x_reduced = x // scaling_factor
@@ -82,7 +81,6 @@ class ZarrDataset(NPYDataset):
         ''' Read zarr file on disk '''
         zarr_path = f'{file_path}/data.zarr'
         slide = zarr.open(zarr_path, mode = 'r')
-        #print(f"Slide dimensions (from Zarr file): {slide.shape}") 
         return slide
 
 class CANVASDataset(ZarrDataset):
@@ -99,9 +97,6 @@ class CANVASDataset(ZarrDataset):
     def __getitem__(self, index):
         image, label, x, y, img_id = super().__getitem__(index)
     
-
-        #print(f"GPT Image shape before indexing: {image.shape}")  # Add this line to check the shape of the image
-        #print(f"GPT Channel indices: {self.channel_idx}")  # Add this line to check the indices being accessed
         # Move channel to first dimension
         if not self.channel_idx is None:
             image = image[self.channel_idx, :, :]
@@ -110,7 +105,7 @@ class CANVASDataset(ZarrDataset):
 
     def get_channel_idx(self, channel_names):
         ''' Get channel index from channel names '''
-        channel_df = pd.read_csv('/gpfs/scratch/ss14424/Brain/channels_20.csv')
+        channel_df = pd.read_csv(CHANNEL_CSV_PATH)
 
         channel_dict = dict(zip(channel_df['marker'], channel_df['channel']))
 
@@ -128,12 +123,8 @@ class SlidesDataset(data.Dataset):
     ''' Dataset for a list of slides '''
 
     def __init__(self, slides_root_path = None, tile_size = None, transform = None, dataset_class = None, use_normalization = True):
-        #print(f'class SlidesDataset was initialized with root path: {slides_root_path}')
         self.slides_root_path = slides_root_path
         self.tile_size = tile_size
-        
-        #print(f'imc_dataset thinks tile_size is {tile_size}')
-        
         self.transform = transform
         # Get id and path for all slides
         slide_ids = self.get_slide_paths(slides_root_path)
@@ -162,41 +153,9 @@ class SlidesDataset(data.Dataset):
         return sum(self.lengths)
 
     def get_common_channel_names(self, root_path):
-        print(f'Root path:{root_path}')
-        with open('/gpfs/scratch/ss14424/Brain/common_channels_20.txt', 'r') as f:
+        with open(CHANNEL_TXT_PATH, 'r') as f:
             channel_names = f.read().splitlines()
         return channel_names
-
-#    def get_normalization_stats(self):
-#        ''' Get normalization stats across samples '''
-#        from tqdm import tqdm
-#        mean = 0
-#        std = 0
-#        stats_path = f'{self.slides_root_path}/../stats'
-#        # Load mean and std if exists
-#        if os.path.exists(f'{stats_path}/mean.npy') and os.path.exists(f'{stats_path}/std.npy'):
-#            mean = np.load(f'{stats_path}/mean.npy')
-#            std = np.load(f'{stats_path}/std.npy')
-#        else:
-#            # Generate random samples with seed
-#            rand_state = np.random.RandomState(42)
-#            rand_idices = rand_state.randint(0, len(self), size = 1000)
-#
-#            n_samples = 0
-#            for i in tqdm(rand_idices):
-#                image, label  = self.__getitem__(i)
-#                mean += image.mean(axis = (1, 2))
-#                std += image.std(axis = (1, 2))
-#                n_samples += 1
-#            mean /= n_samples
-#            std /= n_samples
-#            mean = mean[:, np.newaxis, np.newaxis]
-#            std = std[:, np.newaxis, np.newaxis]
-#            # Save stats
-#            os.makedirs(stats_path, exist_ok = True)
-#            np.save(f'{stats_path}/mean.npy', mean)
-#            np.save(f'{stats_path}/std.npy', std)
-#        return mean, std
 
     def get_normalization_stats(self):
         from tqdm import tqdm
@@ -254,29 +213,17 @@ class SlidesDataset(data.Dataset):
 
         return mean, std
 
-
-
-
-
-
-    
-    #ITS THIS THAT FAILS
     def get_slide_paths(self, slides_root_path):
         ''' Get slides from a directory '''
-        #print(f'get_slide_paths thinks that slides_root_path is: {slides_root_path}')
         slide_ids = []
         slide_channels = []
         slide_channel_dicts = []
         for slide_id in os.listdir(slides_root_path):
             if os.path.isdir(os.path.join(slides_root_path, slide_id)) and not slide_id.startswith('.') and 'V' not in slide_id:
                 mat = zarr.open(f'{slides_root_path}/{slide_id}/data.zarr', mode = 'r')
-                print(f'MAT: {mat.shape}')
                 
-                #print(f'{slides_root_path}/{slide_id}/data.zarr')
-                #channel_df = pd.read_csv(f'{slides_root_path}/{slide_id}/channels.csv')
-
                 parent_directory = os.path.dirname(os.path.dirname(slides_root_path))
-                channel_path = os.path.join(parent_directory, 'channels_20.csv')
+                channel_path = os.path.join(parent_directory, CHANNEL_CSV_PATH)
 
                 channel_df = pd.read_csv(channel_path)
                 channel_dict = dict(zip(channel_df['channel'], channel_df['marker']))
@@ -285,24 +232,19 @@ class SlidesDataset(data.Dataset):
                 slide_ids.append(slide_id)
 
         # Check if all slides have the same channels
-        print(f'Found {len(slide_ids)} slides with {slide_channels} channels')
-
-        common_channels_path = f'/gpfs/scratch/ss14424/Brain/common_channels_20.txt'
-        print(f'IDENT common_channels_path is {common_channels_path}')
-        if not os.path.exists(common_channels_path):
+        if not os.path.exists(CHANNEL_TXT_PATH):
             common_channels = self.get_common_channels(slide_channel_dicts)
             # Save common channels as txt file
-            with open(common_channels_path, 'w') as f:
+            with open(CHANNEL_TXT_PATH, 'w') as f:
                 for channel in common_channels:
                     f.write(f'{channel}\n')
             if len(set(slide_channels)) > 1 or len(set([tuple(channel_dict.values()) for channel_dict in slide_channel_dicts])) > 1:
-                raise Exception(f'All slides must have the same channels, common channel file is written to {common_channels_path}, PLEASE REVIEW')
+                raise Exception(f'All slides must have the same channels, common channel file is written to {CHANNEL_TXT_PATH}, PLEASE REVIEW')
             else:
-                raise Exception(f'All slides DO have the same channels, common channel file is written to {common_channels_path}, PLEASE REVIEW and remove unnecessary channels')
+                raise Exception(f'All slides DO have the same channels, common channel file is written to {CHANNEL_TXT_PATH}, PLEASE REVIEW and remove unnecessary channels')
         return slide_ids
 
     def get_common_channels(self, slide_channel_dicts):
-        print('get_common_channels was called')
         ''' Get common channels for a list of slides '''
         common_markers = [] # Channel dict: channel -> marker
         for channel_dict in slide_channel_dicts:
@@ -311,11 +253,9 @@ class SlidesDataset(data.Dataset):
         return common_markers
 
     def get_slides(self, slide_ids, dataset_class, common_channel_names):
-        ''' Get slides from a list of slide ids '''
         from tqdm import tqdm
         slides_dict = {}
         lengths = []
-        print('Loading slides...')
         for slide_id in tqdm(slide_ids):
             slide_path = os.path.join(self.slides_root_path, slide_id)
             slide = dataset_class(slide_path, self.tile_size, common_channel_names, self.transform)
