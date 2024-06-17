b57cfe55a6675f40a9422482e9d88865f02fa244
epsilonix <sterling.sam@gmail.com>
2024-06-17 17:47:40 -0400

    Update pretrain.sh

--- src/model/pretrain.sh
+++ src/model/pretrain.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH -t 3-00:00:00
-#SBATCH -p gpu4_long,gpu8_long
+#SBATCH -p gpu4_long,gpu8_long,gpu4_medium,gpu8_medium
 #SBATCH -N 1
-#SBATCH --mem=200G
+#SBATCH --mem=300G
 #SBATCH --tasks-per-node=1
-#SBATCH --cpus-per-task=4
-#SBATCH --gres=gpu:4
+#SBATCH --cpus-per-task=10
+#SBATCH --gres=gpu:8
 #SBATCH --job-name=train_20chan_20px
 #SBATCH --output=/gpfs/scratch/ss14424/logs/train_20chan_20px_%j.log
 
@@ -17,7 +17,7 @@ source activate /gpfs/home/ss14424/.conda/envs/canvas-env
 torchrun \
     --standalone \
     --nnodes=1 \
-    --nproc_per_node=4 \
+    --nproc_per_node=8 \
     /gpfs/scratch/ss14424/singlecell/src/model/main_pretrain.py \
         --epoch 801 \
         --batch_size 32 \
