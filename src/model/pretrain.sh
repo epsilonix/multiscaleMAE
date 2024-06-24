5348af2eb329f3322c25a11d2d367b14f55201be
epsilonix <sterling.sam@gmail.com>
2024-06-24 14:25:43 -0400

    a

--- src/model/pretrain.sh
+++ src/model/pretrain.sh
@@ -2,12 +2,12 @@
 #SBATCH -t 3-00:00:00
 #SBATCH -p gpu4_long,gpu8_long,gpu4_medium,gpu8_medium
 #SBATCH -N 1
-#SBATCH --mem=300G
+#SBATCH --mem=200G
 #SBATCH --tasks-per-node=1
-#SBATCH --cpus-per-task=10
+#SBATCH --cpus-per-task=16
 #SBATCH --gres=gpu:8
-#SBATCH --job-name=train_20chan_20px
-#SBATCH --output=/gpfs/scratch/ss14424/logs/train_20chan_20px_%j.log
+#SBATCH --job-name=train_20chan_16px
+#SBATCH --output=/gpfs/scratch/ss14424/logs/train_20chan_16px_%j.log
 
 #unset PYTHONPATH
 #module load anaconda3/gpu/5.2.0
@@ -21,7 +21,7 @@ torchrun \
     /gpfs/scratch/ss14424/singlecell/src/model/main_pretrain.py \
         --epoch 801 \
         --batch_size 32 \
-        --tile_size 20 \
-        --output_dir "/gpfs/scratch/ss14424/Brain/channels_20/cells/model_output_20" \
+        --tile_size 16 \
+        --output_dir "/gpfs/scratch/ss14424/Brain/channels_20/cells_new/model_output_16" \
         --log_dir "/gpfs/scratch/ss14424/logs" \
-        --data_path "/gpfs/scratch/ss14424/Brain/channels_20/cells/img_output_20"
+        --data_path "/gpfs/scratch/ss14424/Brain/channels_20/cells_new/img_output_16"
