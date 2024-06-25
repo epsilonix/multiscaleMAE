6d11eb47f542c76ad1218a887f2a67a2dbd83284
epsilonix <sterling.sam@gmail.com>
2024-06-25 13:56:12 -0400

    Update canvas_base.py

--- src/analysis/canvas_base.py
+++ src/analysis/canvas_base.py
@@ -12,9 +12,9 @@ from tqdm import tqdm
 
 def main():
     # Initialize CANVAS
-    data_path = '/gpfs/scratch/ss14424/Brain/channels_20/cells/img_output_10'
-    model_path = '/gpfs/scratch/ss14424/Brain/channels_20/cells/model_output_20/checkpoint-300.pth'
-    save_path = '/gpfs/scratch/ss14424/Brain/channels_20/cells/analysis_output/'
+    data_path = '/gpfs/scratch/ss14424/Brain/channels_20/cells_new/img_output_20'
+    model_path = '/gpfs/scratch/ss14424/Brain/channels_20/cells_new/model_output_20/checkpoint-500.pth'
+    save_path = '/gpfs/scratch/ss14424/Brain/channels_20/cells_new/analysis_output/'
     canvas = Canvas(model_path, data_path, save_path)
 
 #    #Generate embeddings
