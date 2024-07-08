43560b11cd777312f62adb0c84dd2cc4d5e6986c
epsilonix <sterling.sam@gmail.com>
2024-07-08 13:44:08 -0400

    Update canvas_base_run.sh

--- src/analysis/canvas_base_run.sh
+++ src/analysis/canvas_base_run.sh
@@ -2,10 +2,10 @@
 #SBATCH -t 0-01:00:00
 #SBATCH -p gpu4_long,gpu8_long
 #SBATCH -N 1
-#SBATCH --mem=100G
+#SBATCH --mem=250G
 #SBATCH --tasks-per-node=1
-#SBATCH --cpus-per-task=5
-#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=20
+#SBATCH --gres=gpu:4
 #SBATCH --job-name=infer_MAE
 #SBATCH --output=/gpfs/scratch/ss14424/logs/infer_%j.log
 
@@ -15,5 +15,3 @@ source activate /gpfs/home/ss14424/.conda/envs/canvas-env
 python src/analysis/canvas_base.py
 
 
-
-#srun --pty --time=0-07:00:00 --partition=gpu4_long,gpu8_long --nodes=1 --mem=150G --ntasks-per-node=1 --cpus-per-task=1 --gres=gpu:1 --job-name=inter_canvas bash
