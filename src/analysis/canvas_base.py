95154ec05f27f1068b470b64b0002dfb3e20530f
epsilonix <sterling.sam@gmail.com>
2024-08-06 13:36:44 -0400

    a

--- src/analysis/canvas_base.py
+++ src/analysis/canvas_base.py
@@ -13,7 +13,7 @@ from tqdm import tqdm
 def main():
     # Initialize CANVAS
     data_path = '/gpfs/scratch/ss14424/Brain/channels_37/cells/img_output_16'
-    model_path = '/gpfs/scratch/ss14424/Brain/channels_37/cells/model_output_20/checkpoint-300.pth'
+    model_path = '/gpfs/scratch/ss14424/Brain/channels_37/cells/model_output_20/checkpoint-340.pth'
     save_path = '/gpfs/scratch/ss14424/Brain/channels_37/cells/analysis_output/'
     tile_size = 16
     canvas = Canvas(model_path, data_path, save_path, tile_size)
--- src/analysis/canvas_base_run.sh
+++ src/analysis/canvas_base_run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#SBATCH -t 0-05:00:00
+#SBATCH -t 0-12:00:00
 #SBATCH -p gpu4_long,gpu8_long,gpu4_medium,gpu8_medium,gpu4_short,gpu8_short
 #SBATCH -N 1
-#SBATCH --mem=250G
+#SBATCH --mem=300G
 #SBATCH --tasks-per-node=1
-#SBATCH --cpus-per-task=20
+#SBATCH --cpus-per-task=30
 #SBATCH --gres=gpu:4
 #SBATCH --job-name=infer_MAE
-#SBATCH --output=/gpfs/scratch/ss14424/logs/infer_%j.log
+#SBATCH --output=/gpfs/scratch/ss14424/logs/infer_singlecell%j.log
 
 
 source activate /gpfs/home/ss14424/.conda/envs/canvas-env
