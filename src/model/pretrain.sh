988ccb8e06e4661180b3dace41e5a922819fd709
epsilonix <sterling.sam@gmail.com>
2024-09-04 14:16:06 -0400

    Update pretrain.sh

--- src/model/pretrain.sh
+++ src/model/pretrain.sh
@@ -22,6 +22,6 @@ torchrun \
         --epoch 1001 \
         --batch_size 32 \
         --tile_size 20 \
-        --output_dir "/gpfs/scratch/ss14424/Brain/channels_37/cells_filtered/model_output_20" \
+        --output_dir "/gpfs/scratch/ss14424/Brain/channels_37/cells_filtered/model_output_20_new" \
         --log_dir "/gpfs/scratch/ss14424/logs" \
         --data_path "/gpfs/scratch/ss14424/Brain/channels_37/cells_filtered/img_output_20"
