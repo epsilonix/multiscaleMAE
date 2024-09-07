2fc187a49e6a869f459418c10451f1a207f70608
epsilonix <sterling.sam@gmail.com>
2024-09-07 19:51:54 -0400

    Update pretrain.sh

--- src/model/pretrain.sh
+++ src/model/pretrain.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 #SBATCH -t 3-00:00:00
-#SBATCH -p gpu4_long,gpu8_long,gpu4_medium,gpu8_medium
+#SBATCH -p gpu4_long,gpu8_long,gpu4_medium,gpu8_medium,gpu4_short,gpu8_short
 #SBATCH -N 1
-#SBATCH --mem=200G
+#SBATCH --mem=100G
 #SBATCH --tasks-per-node=1
-#SBATCH --cpus-per-task=20
-#SBATCH --gres=gpu:4
+#SBATCH --cpus-per-task=10
+#SBATCH --gres=gpu:1
 #SBATCH --job-name=train_37chan_20px_all_filtered
 #SBATCH --output=/gpfs/scratch/ss14424/logs/train_37chan_20px_all_filtered_%j.log
 
@@ -17,12 +17,12 @@ source activate /gpfs/data/tsirigoslab/home/ss14424/.conda/envs/canvasenv
 torchrun \
     --standalone \
     --nnodes=1 \
-    --nproc_per_node=4 \
+    --nproc_per_node=1 \
     /gpfs/scratch/ss14424/singlecell/src/model/main_pretrain.py \
         --epoch 1001 \
         --batch_size 32 \
         --tile_size 20 \
-        --output_dir "/gpfs/scratch/ss14424/Brain/channels_37/cells_filtered/model_output_20_new" \
+        --output_dir "/gpfs/scratch/ss14424/Brain/channels_37/cells_blankout/model_output_20" \
         --log_dir "/gpfs/scratch/ss14424/logs" \
-        --data_path "/gpfs/scratch/ss14424/Brain/channels_37/cells_filtered/img_output_20" \
+        --data_path "/gpfs/scratch/ss14424/Brain/channels_37/cells_blankout/img_output_20_train" \
         --blankoutbg
