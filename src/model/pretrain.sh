41f6113ff38ecd8076a6807038d5ae6d57d6b4e5
epsilonix <sterling.sam@gmail.com>
2024-09-09 13:47:24 -0400

    Update pretrain.sh

--- src/model/pretrain.sh
+++ src/model/pretrain.sh
@@ -6,15 +6,13 @@
 #SBATCH --tasks-per-node=1
 #SBATCH --cpus-per-task=30
 #SBATCH --gres=gpu:8
-#SBATCH --job-name=train_37chan_20px_standard
-#SBATCH --output=/gpfs/scratch/ss14424/logs/train_37chan_20px_standard_%j.log
+#SBATCH --job-name=train_37chan_20px_blankoutv2
+#SBATCH --output=/gpfs/scratch/ss14424/logs/train_37chan_20px_blankoutv2_%j.log
 
 #unset PYTHONPATH
 #module load anaconda3/gpu/5.2.0
 source activate /gpfs/data/tsirigoslab/home/ss14424/.conda/envs/canvasenv
 #module unload anaconda3/gpu/5.2.0
-
-export TORCHELASTIC_ERROR_FILE=/gpfs/scratch/ss14424/logs/torch_elastic_error.log
 
 torchrun \
     --standalone \
@@ -27,5 +25,4 @@ torchrun \
         --output_dir "/gpfs/scratch/ss14424/Brain/channels_37/cells_blankout/model_output_20" \
         --log_dir "/gpfs/scratch/ss14424/logs" \
         --data_path "/gpfs/scratch/ss14424/Brain/channels_37/cells_blankout/img_output_20_train" \
-        --blankoutbg \
-        --resume "/gpfs/scratch/ss14424/Brain/channels_37/cells_blankout/model_output_20/checkpoint-40.pth"
+        --blankoutbg
