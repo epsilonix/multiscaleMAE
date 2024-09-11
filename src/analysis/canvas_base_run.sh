51738dd79d31543b5f1c710b5fed5151a90ee585
epsilonix <sterling.sam@gmail.com>
2024-09-11 11:52:56 -0400

    Update canvas_base_run.sh

--- src/analysis/canvas_base_run.sh
+++ src/analysis/canvas_base_run.sh
@@ -2,10 +2,10 @@
 #SBATCH -t 0-8:00:00
 #SBATCH -p gpu4_long,gpu8_long,gpu4_medium,gpu8_medium,gpu4_short,gpu8_short
 #SBATCH -N 1
-#SBATCH --mem=150G
+#SBATCH --mem=70G
 #SBATCH --tasks-per-node=1
 #SBATCH --cpus-per-task=20
-#SBATCH --gres=gpu:4
+#SBATCH --gres=gpu:1
 #SBATCH --job-name=infer_MAE_blankout_limited
 #SBATCH --output=/gpfs/scratch/ss14424/logs/infer_singlecell%j.log
 
