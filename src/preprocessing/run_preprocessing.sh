58a8933819ab6c87d365a9faea8a59a9480e0613
epsilonix <sterling.sam@gmail.com>
2024-09-11 11:10:10 -0400

    Update run_preprocessing.sh

--- src/preprocessing/run_preprocessing.sh
+++ src/preprocessing/run_preprocessing.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
-#SBATCH -t 0-07:00:00
-#SBATCH -p gpu4_long,gpu8_long
+#SBATCH -t 0-12:00:00
+#SBATCH -p gpu4_long,gpu8_long,gpu4_medium,gpu8_medium,gpu4_short,gpu8_short
 #SBATCH -N 1
-#SBATCH --mem=100G
+#SBATCH --mem=200G
 #SBATCH --tasks-per-node=1
-#SBATCH --cpus-per-task=1
-#SBATCH --gres=gpu:1
+#SBATCH --cpus-per-task=40
+#SBATCH --gres=gpu:4
 #SBATCH --job-name=preprocessing_MAE
 #SBATCH --output=/gpfs/scratch/ss14424/logs/preprocess_%j.log
 
 
 #modes are training and inference
-source activate /gpfs/home/ss14424/.conda/envs/canvasenv
+source activate /gpfs/data/tsirigoslab/home/ss14424/.conda/envs/canvasenv
 
 python \
 /gpfs/scratch/ss14424/singlecell/src/preprocessing/preprocess.py \
