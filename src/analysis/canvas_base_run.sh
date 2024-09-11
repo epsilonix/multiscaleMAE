9acd6ee1066201bbb49cafea0943a8893416b8a7
epsilonix <sterling.sam@gmail.com>
2024-09-11 10:42:54 -0400

    lots of workers

--- src/analysis/canvas_base_run.sh
+++ src/analysis/canvas_base_run.sh
@@ -2,9 +2,9 @@
 #SBATCH -t 0-12:00:00
 #SBATCH -p gpu4_long,gpu8_long,gpu4_medium,gpu8_medium,gpu4_short,gpu8_short
 #SBATCH -N 1
-#SBATCH --mem=150G
+#SBATCH --mem=300G
 #SBATCH --tasks-per-node=1
-#SBATCH --cpus-per-task=30
+#SBATCH --cpus-per-task=150
 #SBATCH --gres=gpu:4
 #SBATCH --job-name=infer_MAE
 #SBATCH --output=/gpfs/scratch/ss14424/logs/infer_singlecell%j.log
